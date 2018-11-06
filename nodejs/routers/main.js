f2c431cf29a2938532f9642a1c9aff34d80950b4
xuejun <xuejun.liu@efrei.net>
2018-11-06 15:20:13 +0100

    blog

---
+++ nodejs/routers/main.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var Category=require('../models/Category');
+
+router.get('/',function(req,res,next){
+    Category.find().then(function(categories){
+
+        res.render('main/index',{
+            userInfo:req.userInfo,
+            categories:categories
+        });
+    }); 
+});
+module.exports=router;
---
+++ nodejs/schemas/users.js
@@ -0,0 +1,11 @@
+var mongoose=  require('mongoose');
+
+module.exports=mongoose.Schema({
+    username:String,
+    password:String,
+    isAdmin:{
+        type:Boolean,
+        default:false
+    }
+});
+

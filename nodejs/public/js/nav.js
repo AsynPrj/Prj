7e3980281771b35b132001a9e850e986b1d2caff
xuejun <xuejun.liu@efrei.net>
2018-11-12 14:12:13 +0100

    fix bug

--- nodejs/public/js/nav.js
+++ nodejs/public/js/nav.js
@@ -2,7 +2,7 @@
 var obj=null;
 var As=document.getElementById('topnav').getElementsByTagName('a');
 obj = As[0];
-for(i=1;i<As.length;i++){
+for(var i=1;i<As.length;i++){
     if(window.location.href.indexOf(As[i].href)>=0)
         obj=As[i];
     }

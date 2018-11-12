240285bb47432895cd067140697f708dd0cd9100
YolandaW <yuyao.wei@efrei.net>
2018-11-12 18:51:25 +0100

    update pages prev&next

--- nodejs/public/js/index.js
+++ nodejs/public/js/index.js
@@ -1,21 +1,21 @@
-'use strict'
+'use strict';
 $(function () {
-  var $loginBox = $('#loginBox')
-  var $registerBox = $('#registerBox')
-  var $userInfo = $('#userInfo')
-  var $logout = $('#logout')
+  var $loginBox = $('#loginBox');
+  var $registerBox = $('#registerBox');
+  var $userInfo = $('#userInfo');
+  var $logout = $('#logout');
 
   // default show login box and hide the register box
-  $loginBox.show()
-  $registerBox.hide()
+  $loginBox.show();
+  $registerBox.hide();
   // switch from the login box and register box
   $loginBox.find('a').on('click', function () {
-    $registerBox.show()
-    $loginBox.hide()
+    $registerBox.show();
+    $loginBox.hide();
   })
   $registerBox.find('a').on('click', function () {
-    $loginBox.show()
-    $registerBox.hide()
+    $loginBox.show();
+    $registerBox.hide();
   })
   // register method
   $registerBox.find('#registerBtn').on('click', function () {
@@ -32,16 +32,16 @@ $(function () {
             function (data) {
               $registerBox.find('.colWarning').html(data.message)
               if (!data.code) {
-                $loginBox.show()
-                $registerBox.hide()
+                $loginBox.show();
+                $registerBox.hide();
               }
             },
       error:
             function () {
-              console.log('error')
+              console.log('error');
             }
-    })
-  })
+    });
+  });
 
   $loginBox.find('#loginBtn').on('click', function () {
     $.ajax({
@@ -56,43 +56,12 @@ $(function () {
             function (data) {
               $loginBox.find('.colWarning').html(data.message)
               if (!data.code) {
-                window.location.reload()
+                window.location.reload();
               }
             }
-<<<<<<< HEAD
         });  
     });
     
-    //logout method
-    $logout.on('click',function(){
-        $.ajax({          
-            url:'/api/user/logout',
-            success: 
-            function(data){
-                if(!data.code){
-                 window.location.reload();                  
-                }
-            }
-        });  
-    });
-    
-        //logout method
-        $logoutBackend.on('click',function(){
-            $.ajax({          
-                url:'/api/user/logout',
-                success: 
-                function(data){
-                    if(!data.code){
-                     window.location.replace("https://localhost:8888/");                  
-                    }
-                }
-            });  
-        });
-});
-=======
-    })
-  })
-
   // logout method
   $logout.on('click', function () {
     $.ajax({
@@ -100,10 +69,9 @@ $(function () {
       success:
             function (data) {
               if (!data.code) {
-                window.location.reload()
+                window.location.reload();
               }
             }
-    })
-  })
-})
->>>>>>> a92ed9f62ac1ede59a506cd2a39aba8b683ada02
+    });
+  });
+});
